6a018d7ec61fba8007724fd06a440f0684c05d83
P1zza-L1cht <haruki21corder@gmail.com>
2022-07-17 11:35:13 +0900

    first step whit java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+  public static void main(String arg[]) {
+
+    System.out.println("Hello World with JAVA!!");
+    System.out.println("and Just fun a great life.");
+  }
+}
